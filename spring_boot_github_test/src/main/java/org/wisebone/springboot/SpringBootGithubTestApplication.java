922b1cd840352bb4d9866a3e2e3501fdc1e79016
wisebone <hunjoo.joo@daum.net>
2018-10-21 22:55:45 +0900

    first commit

---
+++ spring_boot_github_test/src/main/java/org/wisebone/springboot/SpringBootGithubTestApplication.java
@@ -0,0 +1,12 @@
+package org.wisebone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubTestApplication.class, args);
+	}
+}
